fbf9295499c38947d44212d674ff153b2e9a6509
Iñaki Ibarrola Atxa <inaki.ibarrola@comalatech.com>
2022-01-11 17:26:06 +0100

    Reduce the amount of conditions in the replacement function

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -10,10 +10,9 @@ export class FizzBuzz {
 
 	private static getReplacement(number: number): string | number {
 		if (number % 3 == 0 && number % 5 == 0) return 'FizzBuzz';
-		if (number % 7 == 0 && number % 3 == 0) return 'Fazz';
-		if (number % 3 == 0) return 'Fizz';
-		if (number % 5 == 0) return 'Buzz';
 		if (number % 7 == 0) return 'Fazz';
+		if (number % 5 == 0) return 'Buzz';
+		if (number % 3 == 0) return 'Fizz';
 
 		return number;
 	}
