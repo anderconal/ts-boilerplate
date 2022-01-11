e74ffcc0e97e8902c2628b6bac70db5265d00489
Iñaki Ibarrola Atxa <inaki.ibarrola@comalatech.com>
2022-01-11 17:06:24 +0100

    Rename paremeter

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,18 +1,18 @@
 export class FizzBuzz {
 	static replace(limit: number): (string | number)[] {
 		const replacedFizzArray: (string | number)[] = [];
-		for (let i = 1; i <= limit; i++) {
-			replacedFizzArray.push(this.getReplacement(i));
+		for (let number = 1; number <= limit; number++) {
+			replacedFizzArray.push(this.getReplacement(number));
 		}
 
 		return replacedFizzArray;
 	}
 
-	private static getReplacement(i: number): string | number {
-		if (i % 3 == 0 && i % 5 == 0) return 'FizzBuzz';
-		if (i % 3 == 0) return 'Fizz';
-		if (i % 5 == 0) return 'Buzz';
+	private static getReplacement(number: number): string | number {
+		if (number % 3 == 0 && number % 5 == 0) return 'FizzBuzz';
+		if (number % 3 == 0) return 'Fizz';
+		if (number % 5 == 0) return 'Buzz';
 
-		return i;
+		return number;
 	}
 }
