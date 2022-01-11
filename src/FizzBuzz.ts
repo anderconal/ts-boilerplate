b66b9e324a795cc9927edcca8088844f95e52200
Iñaki Ibarrola Atxa <inaki.ibarrola@comalatech.com>
2022-01-11 16:51:33 +0100

    Extract function for number replacement

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,17 +1,17 @@
 export class FizzBuzz {
 	static replace(limit: number): (string | number)[] {
-		// const numbersArray: number[] = [];
 		const replacedFizzArray: (string | number)[] = [];
 		for (let i = 1; i <= limit; i++) {
-			if (i % 3 == 0) {
-				replacedFizzArray.push('Fizz');
-			} else if (i % 5 == 0) {
-				replacedFizzArray.push('Buzz');
-			} else {
-				replacedFizzArray.push(i);
-			}
+			replacedFizzArray.push(this.getReplacement(i));
 		}
 
 		return replacedFizzArray;
+	}
+
+	private static getReplacement(i: number): string | number {
+		if (i % 3 == 0) return 'Fizz';
+		if (i % 5 == 0) return 'Buzz';
+
+		return i;
 	}
 }
