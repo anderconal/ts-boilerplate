17db494a963e4116739d528143e904c6a2b33f4c
Andrea Rivera <andrea@comalatech.com>
2022-01-18 17:31:03 +0100

    add rules interface

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,18 +1,77 @@
+interface rulesInterface {
+	meetsRule(number:number): boolean;
+	getReplacement(): string;
+}
+
+class FizzRule implements rulesInterface {
+	meetsRule(number: number): boolean {
+		return number % 3 == 0;
+	}
+
+	getReplacement(): string {
+		return 'Fizz';
+	}
+}
+
+class BuzzRule implements rulesInterface {
+	meetsRule(number: number): boolean {
+		return number % 5 == 0;
+	}
+
+	getReplacement(): string {
+		return 'Buzz';
+	}
+}
+
+ class FazzRule implements rulesInterface {
+ 	meetsRule(number: number): boolean {
+ 		return number % 7 == 0;
+ 	}
+
+	getReplacement(): string {
+		return 'Fazz';
+	}
+}
+ class FizzBuzzRule implements rulesInterface {
+ 	meetsRule(number: number): boolean {
+ 		return (number % 3 == 0 && number % 5 == 0);
+ 	}
+
+	getReplacement(): string {
+		return 'FizzBuzz';
+	}
+}
 export class FizzBuzz {
 	static replace(limit: number): (string | number)[] {
 		const replacedFizzArray: (string | number)[] = [];
 		for (let number = 1; number <= limit; number++) {
-			replacedFizzArray.push(this.getReplacement(number));
+			replacedFizzArray.push(this.replaceValues(number));
 		}
 
 		return replacedFizzArray;
 	}
 
-	private static getReplacement(number: number): string | number {
-		if (number % 3 == 0 && number % 5 == 0) return 'FizzBuzz';
-		if (number % 7 == 0) return 'Fazz';
-		if (number % 5 == 0) return 'Buzz';
-		if (number % 3 == 0) return 'Fizz';
+	private static replaceValues(number: number): string | number {
+
+		const ruleFizzBuzz = new FizzBuzzRule();
+		if(ruleFizzBuzz.meetsRule(number)){
+			return ruleFizzBuzz.getReplacement();
+		}
+
+		const ruleFazz = new FazzRule();
+		 if(ruleFazz.meetsRule(number)){
+		 	return ruleFazz.getReplacement();
+		 }
+
+		const ruleBuzz = new BuzzRule();
+		if(ruleBuzz.meetsRule(number)){
+			return ruleBuzz.getReplacement();
+		}
+		
+		const ruleFizz = new FizzRule();
+		if(ruleFizz.meetsRule(number)){
+			return ruleFizz.getReplacement();
+		}
 
 		return number;
 	}
