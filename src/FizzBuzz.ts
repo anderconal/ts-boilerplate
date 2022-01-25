4d87018ac77d48c4ad8a2d0eb2c6c39d67df2d60
Maria <maria@comalatech.com>
2022-01-25 16:35:15 +0100

    implemets rules classes

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -9,25 +9,52 @@ export class FizzBuzz {
 	}
 
 	private static getReplacement(number: number): string | number {
-		if (number % 3 == 0 && number % 5 == 0) return 'FizzBuzz';
-		if (number % 7 == 0) return 'Fazz';
-		if (number % 5 == 0) return 'Buzz';
-		if (number % 3 == 0) return 'Fizz';
+		const fizzRule = new FizzRule();
+		const buzzRule = new BuzzRule();
+		const fazzRule = new FazzRule();
+		const fizzBuzzRule = new FizzBuzzRule();
+
+		if (fizzBuzzRule.isDivisible(number)) return fizzBuzzRule.getMessage();
+		if (fazzRule.isDivisible(number)) return fazzRule.getMessage();
+		if (buzzRule.isDivisible(number)) return buzzRule.getMessage();
+		if (fizzRule.isDivisible(number)) return fizzRule.getMessage();
 
 		return number;
 	}
 
+	// TODO talk with Iñaki
 	/*private static getSolidReplacement(number: number): string | number {
-		const ruleArray: RuleInterface[] = [];
+		const ruleArray: RuleInterface[] = [FizzRule];
 		for (const rule of ruleArray) {
 			if (rule.isDivisible(number)) {
-
+				retun rule.getMessage()
 			}
 		}
 
 		return number;
 	}*/
 }
+
+export class FazzRule implements RuleInterface {
+	getMessage(): string {
+		return 'Fazz'
+	}
+
+	isDivisible(number: number): boolean {
+		return number % 7 == 0;
+	}
+}
+
+export class BuzzRule implements RuleInterface {
+	getMessage(): string {
+		return 'Buzz'
+	}
+
+	isDivisible(number: number): boolean {
+		return number % 5 == 0;
+	}
+}
+
 export class FizzRule implements RuleInterface {
 	getMessage(): string {
 		return 'Fizz'
@@ -35,6 +62,16 @@ export class FizzRule implements RuleInterface {
 
 	isDivisible(number: number): boolean {
 		return number % 3 == 0;
+	}
+}
+
+export class FizzBuzzRule implements RuleInterface {
+	getMessage(): string {
+		return 'FizzBuzz'
+	}
+
+	isDivisible(number: number): boolean {
+		return (number % 3 == 0 && number % 5 == 0);
 	}
 }
 
--- src/Rule.spec.ts
+++ src/Rule.spec.ts
@@ -1,8 +1,23 @@
-import { FizzRule } from './FizzBuzz';
+import { FizzRule, BuzzRule, FazzRule, FizzBuzzRule } from './FizzBuzz';
 
 describe('Rules Testing', () => {
 	it('Get Fizz ', () => {
 		const fizzRule = new FizzRule();
 		expect(fizzRule.getMessage()).toBe('Fizz');
+	});
+
+	it('Get Buzz ', () => {
+		const buzzRule = new BuzzRule();
+		expect(buzzRule.getMessage()).toBe('Buzz');
+	});
+
+	it('Get Fazz ', () => {
+		const fazzRule = new FazzRule();
+		expect(fazzRule.getMessage()).toBe('Fazz');
+	});
+
+	it('Get FizzBuzz ', () => {
+		const fizzBuzzRule = new FizzBuzzRule();
+		expect(fizzBuzzRule.getMessage()).toBe('FizzBuzz');
 	});
 });
